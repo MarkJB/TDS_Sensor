2d5d05f65a8e7cc42c18ffddf53713b60e2542eb
MarkJB <mark@sub-design.co.uk>
2018-10-07 18:42:59 +0100

    Update licene in tds_sensor.c

--- main/tds_sensor.c
+++ main/tds_sensor.c
@@ -1,10 +1,8 @@
-/* Hello World Example
+/* TDS Sensor Example
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
+   Mark Benson 2018
 
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
+   Licence: MIT (or whatever is used by the rest of this project)
 */
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
